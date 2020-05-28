e9d0217c9b430873512f1dffeaf0ad9f13382e2f
Dumaru <robinsonmu@unisabana.edu.co>
2020-05-28 01:46:35 -0500

    Tiempo

--- Data Structures and Algoritms/Assets/Scripts/UI/PauseMenu.cs
+++ Data Structures and Algoritms/Assets/Scripts/UI/PauseMenu.cs
@@ -7,7 +7,7 @@ public class PauseMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Time.timeScale = 0;
     }
 
     // Update is called once per frame
